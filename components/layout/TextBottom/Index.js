fbdfdf05b595eb8ad04fecc3d89bbf31bfaeb5fb
Nyura <taxisoopya@hotmail.fr>
2017-08-23 23:25:21 +0200

    oublie
    oublie

---
+++ components/layout/TextBottom/Index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Col } from 'react-bootstrap';
+import styles from './Index.scss';
+
+export default class TextBottom extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
