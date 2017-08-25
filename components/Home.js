ce3d73eae640a06839566553c686a1e24eb29dbb
Alexis courapied <taxisoopya@hotmail.fr>
2017-08-25 13:27:41 +0200

    modification video
    modification video

--- components/Home.js
+++ components/Home.js
@@ -6,7 +6,7 @@ import styles from './Home.scss';
 import NavigationLeft from './layout/navigationLeft/';
 import NavigationTop from './layout/NavigationTop/';
 
-import { Grid, Row, Col, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Button, FormControl } from 'react-bootstrap';
 import type { videoStateType } from '../reducers/video';
 import ReactPlayer from 'react-player'
 
@@ -28,31 +28,46 @@ export default class Home extends Component {
   onProgress = state => {
     if(state.loaded) {
       this.setState({
-        loaded: state.loaded
+        loaded: state.loaded,
+        url: ""
       })
     }
     // We only want to update time slider if we are not currently seekin
   }
+
   render() {
     const { loadURL, setVolume, path, video } = this.props;
-    const { loaded } = this.state;
+    const { loaded, url } = this.state;
     return (
       <div data-tid="container">
         <NavigationLeft path={path}/>
         <NavigationTop />
-        <div className={styles.marginLeft+' container-fluid'}>
+        <div id={"container"} className={'container-fluid'}>
           <ReactPlayer
             ref={player => { this.player = player }}
             className='react-player'
             url={video.url}
             volume={video.volume}
-            controls={false} //Permet d'avoir le control natif
             onProgress={this.onProgress}
+            controls={false} //Permet d'avoir le control natif
           />
-          <Button onClick={() => loadURL("https://www.youtube.com/watch?v=jb1-3DvtrmE")}>Video</Button>
-          <Button onClick={() => setVolume(video.volume + 0.1)}>up</Button>
-          <Button onClick={() => setVolume(video.volume - 0.1)}>down</Button>
-          <progress max={1} value={loaded} />
+          <Row>
+            <Col lg={12} md={12} xs={12}>
+            <FormControl
+              type="text"
+              placeholder="Entre une URL (Video interne en cours de développement)"
+              onChange={e => this.setState({url:e.target.value})}
+            />
+              <Button onClick={() => loadURL(this.state.url)}>Load la vidéo</Button>
+              <Button onClick={() => setVolume(video.volume + 0.1)}>up</Button>
+              <Button onClick={() => setVolume(video.volume - 0.1)}>down</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={12} md={12} xs={12}>
+              Loading de la vidéo : <progress max={1} value={loaded} />
+            </Col>
+          </Row>
         </div>
       </div>
     );
